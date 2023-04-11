2094b0784029e1baadf54e9faadf552fe7e28389
Diva <Diva3693@gmail.com>
2023-04-12 03:37:26 +0700

    membuat konfigurasi form sirkulasi di sirkulasi.php

---
+++ tugas1/sirkulasi.php
@@ -0,0 +1,17 @@
+<?php
+include "config.php";
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+$id_buku = $_POST['id_buku'];
+
+$query = "INSERT INTO sirkulasi(firstName, lastName, email, id_buku) VALUES ('$fname', '$lname', '$email', '$id_buku')";
+$result = $conn->query($query);
+
+if ($result) {
+    echo "Peminjaman berhasil dilakukan.";
+}else{
+    echo "Data belum benar, silahkan coba lagi.";
+}
+?>
