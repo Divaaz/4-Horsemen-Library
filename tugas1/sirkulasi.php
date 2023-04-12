659e63eb67d7dfa0e673f1aaf6f29f163477a195
Zzeroanne <anisadekta@gmail.com>
2023-04-12 08:02:16 +0700

    memperbaiki query sirkulasi peminjaman

--- tugas1/sirkulasi.php
+++ tugas1/sirkulasi.php
@@ -6,12 +6,12 @@ $lname = $_POST['lname'];
 $email = $_POST['email'];
 $id_buku = $_POST['id_buku'];
 
-$query = "INSERT INTO sirkulasi(firstName, lastName, email, id_buku) VALUES ('$fname', '$lname', '$email', '$id_buku')";
+$query = "INSERT INTO sirkulasi(firstName, lastName, email, id_buku, tgl_pinjam) VALUES ('$fname', '$lname', '$email', '$id_buku', NOW())";
 $result = $conn->query($query);
 
 if ($result) {
     echo "Peminjaman berhasil dilakukan.";
-}else{
+} else {
     echo "Data belum benar, silahkan coba lagi.";
 }
 ?>
